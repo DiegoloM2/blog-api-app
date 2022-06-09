6b342b7a5aa2e727b7edbec5b6b6115b5bce3ed7
DiegoloM2 <diego.moreno.manes@hotmail.com>
2022-06-09 13:53:10 +0200

    Added Documentation through django's default documentation API

--- blog/urls.py
+++ blog/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 
 schema_view = get_schema_view(title = "Blog API")
 
@@ -26,5 +27,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/rest-auth/', include("rest_auth.urls")),
     path('api/v1/rest-auth/registration', include('rest_auth.registration.urls')),
+    path('docs/', include_docs_urls(title = "Blog API")),
     path('schema/', schema_view),
 ]
